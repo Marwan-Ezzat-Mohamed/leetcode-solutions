9f250a146f9622df5022d1e12eac11aaffe38dbb
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2022-09-23 09:08:52 +0200

    Time: 283 ms (5.06%), Space: 54.9 MB (6.71%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(auto x:nums)
+        {
+            s.insert(x);
+        }
+        return !(s.size()==nums.size());
+    }
+};
