05ce19defb7650dfca153b9022327d57d6dc3552
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-09-10 00:25:28 +0300

    Time: 12 ms (40.35%), Space: 15.8 MB (12.18%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        unordered_set<int>st;
+        for(auto x : numbers)st.insert(x);
+        
+
+        for (auto x = st.begin(); x != st.end(); ++x ){
+            int search = target - (*x);
+        
+
+            auto it = st.find(search);
+            
+            
+
+            if (it != st.end()){
+                vector<int> ans;
+                
+                int index1= lower_bound(numbers.begin(), numbers.end(), *it)
+                - numbers.begin();
+                
+                int index2= upper_bound(numbers.begin(), numbers.end(), *x)
+                - numbers.begin();
+                ans.push_back(index1 +1);
+                ans.push_back(index2 );
+                sort(ans.begin(),ans.end());
+                return ans;
+            }
+   
+        }
+              vector<int> ans;
+        return ans;
+        
+    }
+};
