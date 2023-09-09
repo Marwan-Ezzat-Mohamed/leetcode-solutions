9bb813e676116f064de61e9356d38259c773847a
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-09-09 23:52:10 +0300

    Time: 124 ms (65.79%), Space: 67 MB (55.48%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> st;
+        for (auto x: nums)st.insert(x);
+        
+        int ans=0;
+        
+        for ( auto it = st.begin(); it != st.end(); ++it ){
+            int val = *it;
+            if (st.find(val-1)==st.end()){
+                //then we have the smallest element in the sequence
+                int len=1;
+                int cur=val;
+                while(st.find(cur+1)!=st.end()){
+                    len++;
+                    cur++;
+                }
+                ans=max(ans,len);
+                
+            }
+        }   
+        
+       
+        return  ans;
+    }
+};
