b329f199a2a848c9889765e500eb4b469a4ad7cf
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-09-10 00:38:33 +0300

    Time: 11 ms (57.73%), Space: 15.7 MB (12.18%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,36 +1,19 @@
 #include <algorithm>
 
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        
-        unordered_set<int>st;
-        for(auto x : numbers)st.insert(x);
-        
+  public: vector < int > twoSum(vector < int > & numbers, int target) {
 
-        for (auto x = st.begin(); x != st.end(); ++x ){
-            int search = target - (*x);
-        
+    int l = 0, r = numbers.size() - 1;
 
-            auto it = st.find(search);
-
-            if (it != st.end()){
-                vector<int> ans;
-                
-                int index1= lower_bound(numbers.begin(), numbers.end(), *it)
-                - numbers.begin();
-                
-                int index2= upper_bound(numbers.begin(), numbers.end(), *x)
-                - numbers.begin();
-                ans.push_back(index1 +1);
-                ans.push_back(index2 );
-                sort(ans.begin(),ans.end());
-                return ans;
-            }
-   
-        }
-              vector<int> ans;
-        return ans;
-        
+    while (numbers[l] + numbers[r] != target) {
+      if (numbers[l] + numbers[r] < target) l++;
+      else r--;
     }
+
+    return {
+      l + 1,
+      r + 1
+    };
+
+  }
 };
