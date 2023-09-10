8bf2821bbc87c6baca0928a701e51c6ffd15eab7
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-09-10 21:51:26 +0300

    Time: 8 ms (58.80%), Space: 7.6 MB (29.10%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -4,23 +4,23 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if(s1.size()>s2.size())return false;
-        unordered_map<char,int>mp,mp2;
+        unordered_map<int,int>mp,mp2;
         for(auto x : s1){
-            mp[x]++;
+            mp[x-'a']++;
         }
         
         for(int i=0;i<s1.size();i++){
-            mp2[s2[i]]++;
+            mp2[s2[i]-'a']++;
         }
         
         int l = 0,r=s1.size()-1;
         while(r>=l and r<s2.size()){
             if(l!=0){
-                mp2[s2[r]]++;
+                mp2[s2[r]-'a']++;
             }
             bool valid = true;
             for(auto kvp : mp){
-                char key = kvp.first;
+                int key = kvp.first;
                 int val = kvp.second;
                 if (mp2[key]!=val){
                     valid = false;
@@ -30,7 +30,7 @@ public:
             if (valid){
                 return true;
             }
-            mp2[s2[l++]]--;
+            mp2[s2[l++]-'a']--;
             r++;
          
         }
