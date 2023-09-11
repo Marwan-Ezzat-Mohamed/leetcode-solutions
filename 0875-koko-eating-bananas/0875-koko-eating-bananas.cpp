2de57daa077cea8e9a0c6b8815cb9fcc03c99e6a
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-09-11 04:29:03 +0300

    Time: 38 ms (63.88%), Space: 18.9 MB (51.12%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+
+    int getReq(vector < int > & piles, int num) {
+      int cnt = 0;
+      for (auto x: piles) {
+        int op = ceil((double) x / (double) num);
+        cnt += op;
+      }
+
+      return cnt;
+    }
+  int minEatingSpeed(vector < int > & piles, int h) {
+    double sum = 0;
+    int mx = 0;
+    for (auto x: piles) {
+      sum += x;
+      mx = max(mx, x);
+    }
+
+    int initial = ceil(sum / (double) h);
+    int l = initial, r = mx;
+    int ans = 1e9;
+    int mid;
+    while (l < r) {
+      mid = (l + r) >> 1;
+      int req = getReq(piles, mid);
+      if (req > h) {
+        l = mid + 1;
+
+      } else {
+        r = mid;
+      }
+
+    }
+    return r;
+  }
+};
