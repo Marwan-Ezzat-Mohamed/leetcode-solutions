58aa98b9c4212d07db25d11e8d008e6278b7c7f4
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-09-12 08:14:24 +0300

    Time: 23 ms (51.16%), Space: 14.5 MB (26.96%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     
-     bool hasCycle(vector<vector<int>> &adj,int node, vector<int>&vis, vector<int>dfsVisited){
+     bool hasCycle(vector<vector<int>> &adj,int node, vector<int>&vis){
         
         vis[node]=1;
-        dfsVisited[node]=1;
-        
+
         for(auto child : adj[node]){
             if(!vis[child]){
                 
-                if(hasCycle(adj,child, vis, dfsVisited)){
+                if(hasCycle(adj,child, vis)){
                     return true;   
                 }
                  
@@ -34,11 +33,10 @@ public:
         }
  
         vector<int> vis(numCourses, 0);
-        vector<int> dfsVisited(numCourses, 0);
         
         for(int i = 0; i < numCourses; i++) {
             if(!vis[i]) {
-                if(hasCycle(adj,i, vis, dfsVisited)) {
+                if(hasCycle(adj,i, vis)) {
                     return false;
                 }
             }
