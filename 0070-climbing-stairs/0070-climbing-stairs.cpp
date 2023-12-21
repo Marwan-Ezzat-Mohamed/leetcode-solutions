70e38ba6592bb681048ae772e546e11325f7c686
Marwan Ezzat Mohamed <60528935+Marwan-Ezzat-Mohamed@users.noreply.github.com>
2023-12-21 19:51:36 +0200

    Time: 0 ms (100.00%), Space: 6.3 MB (70.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
-    int dp[46];
     
     int climbStairs(int n) {
-        if (n<=3)return n;
-        if (dp[n])return dp[n];
-        dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        int dp[46];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        for (int i=4;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 };
